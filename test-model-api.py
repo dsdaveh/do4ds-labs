5cf64328ede1a5a67efce12bb506112a26dd6569
Dave Hurst <david@hurst.com>
2024-10-21 10:46:01 -0700

    update

--- test-model-api.py
+++ test-model-api.py
@@ -1,8 +1,11 @@
 import pins
 import vetiver
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 b = pins.board_folder('data/model', allow_pickle_read = True)
 v = vetiver.VetiverModel.from_pin(b, 'penguin_model')
 
-app = vetiver.VetiverAPI(v, check_prototype = True)
+app = vetiver.VetiverAPI(v, check_prototype = False)
 app.run(port = 8080)
